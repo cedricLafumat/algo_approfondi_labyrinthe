b8cdd269614a6273dcd57cb48f3aae9bf490b7d6
cedricLafumat <cedricLafumat@le-campus-numerique.fr>
2020-09-25 12:59:20 +0200

    Ajout de l'algo profondeur iteratif fini

--- laby_deep_iteratif.py
+++ laby_deep_iteratif.py
@@ -6,24 +6,31 @@ labyrinthe = [[00, -1, 00, 00, 00, 00, 00],
               [00, -1, 00, 00, 00, -1, 00]]
 
 
-movecount = 0
-
-
 def displaylaby(labyrinthe):
     for y in range(0, 6):
         print(labyrinthe[y])
 
 
-def move(actualposition, nextposition):
-    global movecount
+def move(actualposition):
+    movecount = 0
+    historic = []
 
     if movecount == 0:
-        movecount += 1
-        labyrinthe[actualposition[0]][actualposition[1]] = movecount
         print("je commence à la position {}".format(actualposition))
-        displaylaby(labyrinthe)
 
     while labyrinthe[actualposition[0]][actualposition[1]] != 'G':
+
+        # si ma position à un marqueur, je reviens à ma position précédente
+        if (labyrinthe[actualposition[0]][actualposition[1]]) != 0:
+            del historic[len(historic) - 1]
+            actualposition = historic[len(historic) - 1]
+            print("je reviens à la position {} ".format(actualposition))
+        # sinon je mets un marqueur
+        else:
+            movecount += 1
+            labyrinthe[actualposition[0]][actualposition[1]] = movecount
+            displaylaby(labyrinthe)
+
         # si la case au SUD est dans le labyrinthe et que la case est vide ou G
         if actualposition[0] + 1 <= len(labyrinthe) - 1 and ((labyrinthe[actualposition[0] +1][actualposition[1]] == 0) or
                                                              (labyrinthe[actualposition[0] +1][actualposition[1]] == 'G')):
@@ -31,13 +38,11 @@ def move(actualposition, nextposition):
             nextposition = actualposition[0]+1, actualposition[1]
             print("je me déplace à la position {}".format(nextposition))
             actualposition = nextposition
-            movecount += 1
+            historic.append(actualposition)
             if labyrinthe[actualposition[0]][actualposition[1]] == 'G':
                 print("j'ai gagné en {} coups à la position {}".format(movecount, actualposition))
+                print("historique de déplacement : {}".format(historic))
                 break
-            # marquage du passage
-            labyrinthe[actualposition[0]][actualposition[1]] = movecount
-            displaylaby(labyrinthe)
 
         # si la case à l'EST est dans le labyrinthe et que la case est vide ou G
         elif actualposition[1] + 1 <= len(labyrinthe[0]) - 1 and ((labyrinthe[actualposition[0]][actualposition[1] + 1] == 0) or
@@ -46,13 +51,11 @@ def move(actualposition, nextposition):
             nextposition = actualposition[0], actualposition[1] +1
             print("je me déplace à la position {}".format(nextposition))
             actualposition = nextposition
-            movecount += 1
+            historic.append(actualposition)
             if labyrinthe[actualposition[0]][actualposition[1]] == 'G':
                 print("j'ai gagné en {} coups à la position {}".format(movecount, actualposition))
+                print("historique de déplacement : {}".format(historic))
                 break
-            # marquage du passage
-            labyrinthe[actualposition[0]][actualposition[1]] = movecount
-            displaylaby(labyrinthe)
 
         # si la case au NORD est dans le labyrinthe et que la case est vide ou G
         elif actualposition[0] - 1 >= 0 and ((labyrinthe[actualposition[0] - 1][actualposition[1]] == 0) or
@@ -61,13 +64,11 @@ def move(actualposition, nextposition):
             nextposition = actualposition[0] -1, actualposition[1]
             print("je me déplace à la position {}".format(nextposition))
             actualposition = nextposition
-            movecount += 1
+            historic.append(actualposition)
             if labyrinthe[actualposition[0]][actualposition[1]] == 'G':
                 print("j'ai gagné en {} coups à la position {}".format(movecount, actualposition))
+                print("historique de déplacement : {}".format(historic))
                 break
-            # marquage du passage
-            labyrinthe[actualposition[0]][actualposition[1]] = movecount
-            displaylaby(labyrinthe)
 
         # si la case à l'OUEST est dans le labyrinthe et que la case est vide ou G
         elif (actualposition[1] - 1) >= 0 and ((labyrinthe[actualposition[0]][actualposition[1] - 1] == 0) or
@@ -76,18 +77,15 @@ def move(actualposition, nextposition):
             nextposition = actualposition[0], actualposition[1] -1
             print("je me déplace à la position {}".format(nextposition))
             actualposition = nextposition
-            movecount += 1
+            historic.append(actualposition)
             if labyrinthe[actualposition[0]][actualposition[1]] == 'G':
                 print("j'ai gagné en {} coups à la position {}".format(movecount, actualposition))
+                print("historique de déplacement : {}".format(historic))
                 break
-            # marquage du passage
-            labyrinthe[actualposition[0]][actualposition[1]] = movecount
-            displaylaby(labyrinthe)
     return
 
 
 if __name__ == '__main__':
     startposition = [0, 0]
-    nextposition = [0, 0]
     displaylaby(labyrinthe)
-    move(startposition, nextposition)
+    move(startposition)
